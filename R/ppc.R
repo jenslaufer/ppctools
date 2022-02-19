2d63ec352e6486b8e2b9d1d07acd7a21706b631c
Jens Laufer <jenslaufer@gmail.com>
2022-02-19 15:35:05 +0100

    added more functions

--- R/ppc.R
+++ R/ppc.R
@@ -2,34 +2,57 @@ cost_per_conversion <- function(cpc, cr) {
   cpc / cr
 }
 
-num_of_conversions <- function(ad_budget, cpc, cr) {
+num_of_conversions_for_ad_budget <- function(ad_budget, cpc, cr) {
   ad_budget / cost_per_conversion(cpc, cr)
 }
 
-revenue <- function(ad_budget, product_cost, cpc, cr) {
-  num_of_conversions(ad_budget, cpc, cr) * product_cost
-}
-
-profit <- function(ad_budget, product_cost, cpc, cr) {
-  revenue(ad_budget, product_cost, cpc, cr) - ad_budget
-}
-
-ad_budget_for_profit <- function(profit, product_cost, cpc, cr) {
-  profit * cost_per_conversion(cpc, cr) / (product_cost - cost_per_conversion(cpc, cr))
-}
-
-traffic_for_profit <- function(profit, product_cost, cpc, cr, ctr) {
-  ad_budget_for_profit(profit, product_cost, cpc, cr) / (ctr * cpc)
-}
-
-profit_for_specified_traffic  <-
-  function(traffic, cpc, product_cost, cr, ctr) {
-    traffic_to_site <- traffic * ctr
-    ad_cost <- traffic_to_site * cpc
-    profit(ad_cost, product_cost, cpc, cr)
+ad_budget_for_num_of_conversions <-
+  function(num_of_conversions, cpc, cr) {
+    num_of_conversions / cr * cpc
   }
 
-product_cost <- function(profit_ad_cost_ratio, cpc, cr) {
+revenue_for_ad_budget <-
+  function(ad_budget, net_product_price, cpc, cr) {
+    num_of_conversions_for_ad_budget(ad_budget, cpc, cr) * net_product_price
+  }
+
+profit_for_ad_budget <-
+  function(ad_budget, net_product_price, cpc, cr) {
+    revenue_for_ad_budget(ad_budget, net_product_price, cpc, cr) - ad_budget
+  }
+
+ad_budget_for_profit <-
+  function(profit, net_product_price, cpc, cr) {
+    profit * cost_per_conversion(cpc, cr) / (net_product_price - cost_per_conversion(cpc, cr))
+  }
+
+volume_for_profit <-
+  function(profit, net_product_price, cpc, cr, ctr) {
+    ad_budget_for_profit(profit, net_product_price, cpc, cr) / (ctr * cpc)
+  }
+
+num_of_conversions <- function(volume, cr, ctr) {
+  volume * ctr * cr
+}
+
+product_price_for_profit <- function(profit, volume, cpc, cr, ctr) {
+  num_of_conversions <- num_of_conversions(volume, cr, ctr)
+  ad_cost <- cost_per_conversion(cpc, cr) * num_of_conversions
+  
+  gross <- profit + ad_cost
+  
+  gross / num_of_conversions
+}
+
+profit_for_specified_volume <-
+  function(volume, cpc, net_product_price, cr, ctr) {
+    num_of_conversions <- num_of_conversions(volume, cr, ctr)
+    ad_cost <- volume * ctr * cpc
+    
+    num_of_conversions * net_product_price - ad_cost
+  }
+
+net_product_price <- function(profit_ad_cost_ratio, cpc, cr) {
   ad_budget <- 1 / profit_ad_cost_ratio
-  (1 + ad_budget) / num_of_conversions(ad_budget, cpc, cr)
+  (1 + ad_budget) / num_of_conversions_for_ad_budget(ad_budget, cpc, cr)
 }
